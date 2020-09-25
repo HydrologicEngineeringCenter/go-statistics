40d32b53d365952d1b94fc4cbfde33ee572ba903
Will Lehman <william.p.lehman@usace.army.mil>
2020-09-24 19:15:30 -0700

    adding normal distribution and implementing inlineStats on ProductMoments

--- data/productMoments.go
+++ data/productMoments.go
@@ -1,5 +1,7 @@
 package data
 
+import "math"
+
 type ProductMoments struct {
 	Min            float64
 	Max            float64
@@ -8,25 +10,26 @@ type ProductMoments struct {
 	SampleVariance float64
 }
 
-/*
 func (pm *ProductMoments) AddObservation(value float64) {
-	if value != nil {
-		if pm.SampleSize == 0 {
+	if pm.SampleSize == 0 {
+		pm.Max = value
+		pm.Min = value
+		pm.Mean = value
+		pm.SampleVariance = 0
+		pm.SampleSize = 1
+	} else {
+		if value > pm.Max {
 			pm.Max = value
+		} else if value < pm.Min {
 			pm.Min = value
-			pm.Mean = value
-			pm.SampleVariance = 0
-			pm.SampleSize = 1
-		} else {
-			if value > pm.Max {
-				pm.Max = value
-			} else if value < pm.Min {
-				pm.Min = value
-			}
-			pm.SampleSize += 1
-			pm.SampleVariance = ((((pm.SampleSize - 2) / (pm.SampleSize - 1)) * pm.SampleVariance) + ((value-pm.Mean)^2)/pm.SampleSize)
-			pm.Mean = pm.Mean + ((value - pm.Mean) / pm.SampleSize)
 		}
+		pm.SampleSize += 1
+		pm.SampleVariance = ((float64((pm.SampleSize-2)/(pm.SampleSize-1)) * pm.SampleVariance) + (math.Pow((value-pm.Mean), 2))/float64(pm.SampleSize))
+		pm.Mean = pm.Mean + ((value - pm.Mean) / float64(pm.SampleSize))
 	}
 }
-*/
+func (pm *ProductMoments) AddObservations(values []float64) {
+	for _, val := range values {
+		pm.AddObservation(val)
+	}
+}
---
+++ statistics/normalDistribution.go
@@ -0,0 +1,43 @@
+package statistics
+
+import "math"
+
+type NormalDistribution struct {
+	Mean              float64
+	StandardDeviation float64
+}
+
+func (n NormalDistribution) InvCDF(probability float64) float64 {
+	if probability == .5 {
+		return n.Mean
+	}
+	if probability <= 0 {
+		probability = .000000000000001
+	}
+	if probability >= 1 {
+		probability = .999999999999999
+	}
+	i := -1
+	if probability > .5 {
+		probability = 1 - probability
+		i = 1
+	}
+	t := math.Sqrt(math.Log(1 / (probability * probability)))
+	c0 := 2.515517
+	c1 := 0.802853
+	c2 := 0.010328
+	d1 := 1.432788
+	d2 := 0.189269
+	d3 := 0.001308
+	tsqrd := t * t
+	x := t - (c0+c1*t+c2*(tsqrd))/(1+d1*t+d2*(tsqrd)+d3*(t*tsqrd))
+	x = float64(i) * x
+	return n.Mean + x*n.StandardDeviation
+}
+func (n NormalDistribution) CDF(value float64) float64 {
+	return math.Erfc(-(value-n.Mean)/(n.StandardDeviation*math.Sqrt2)) / 2
+}
+func (n NormalDistribution) PDF(value float64) float64 {
+	z := value - n.Mean
+	return math.Exp(-z*z/(2*n.StandardDeviation*n.StandardDeviation)) * ((1 / math.Sqrt(2*math.Pi)) / n.StandardDeviation)
+}
