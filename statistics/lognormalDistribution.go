2dfda139709188d43491ea4abd95d02df7faa388
Will Lehman <william.p.lehman@usace.army.mil>
2020-09-24 20:39:33 -0700

    first shot at logNormal

---
+++ statistics/lognormalDistribution.go
@@ -0,0 +1,21 @@
+package statistics
+
+import "math"
+
+type LogNormalDistribution struct {
+	Mean              float64
+	StandardDeviation float64
+}
+
+func (n LogNormalDistribution) InvCDF(probability float64) float64 {
+	z := NormalDistribution{Mean: 0, StandardDeviation: 1}
+	return math.Exp(n.Mean + z.InvCDF(probability)*n.StandardDeviation)
+}
+func (n LogNormalDistribution) CDF(value float64) float64 {
+	z := NormalDistribution{Mean: n.Mean, StandardDeviation: n.StandardDeviation}
+	return z.CDF(math.Log(value))
+}
+func (n NormalDistribution) PDF(value float64) float64 {
+	z := NormalDistribution{Mean: n.Mean, StandardDeviation: n.StandardDeviation}
+	return z.PDF(math.Log(value))
+}
