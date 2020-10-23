d09c0ddf657ec818df868ef0ae15315607a17cb5
Will Lehman <william.p.lehman@usace.army.mil>
2020-10-22 21:31:57 -0700

    fixing the inline histogram to function properly - adding tests

--- data/inline-histogram.go
+++ data/inline-histogram.go
@@ -1,37 +1,49 @@
 package data
 
-import "math"
+import (
+	"math"
+)
 
-type InlineHistogram struct {
-	BinWidth float64
-	Bins     []int64
-	MinValue float64
-	MaxValue float64
+type inlineHistogram struct {
+	binWidth float64
+	bins     []int64
+	minValue float64
+	maxValue float64
 }
 
-func (ih *InlineHistogram) AddObservation(value float64) {
-	if ih.MaxValue > value && value > ih.MinValue {
+func Init(binwidth float64, minval float64, maxval float64) *inlineHistogram {
+	val := math.Ceil((maxval - minval) / binwidth)
+	b := make([]int64, int(val))
+	maxval = minval + math.Floor(val*binwidth)
+	ih := inlineHistogram{bins: b, binWidth: binwidth, minValue: minval, maxValue: maxval}
+	return &ih
+}
+func (ih *inlineHistogram) AddObservation(value float64) {
+	if ih.maxValue > value && value > ih.minValue {
 		//index it to the right location
-		index := float64(len(ih.Bins)) * (value - ih.MinValue) / (ih.MaxValue - ih.MinValue)
+		index := float64(len(ih.bins)) * (value - ih.minValue) / (ih.maxValue - ih.minValue)
 		//increment by 1
-		ih.Bins[int(index)] += 1 //truncation towards zero
+		ih.bins[int(index)] += 1 //truncation towards zero
 	} else {
 		//bins need to change.
-		if ih.MaxValue < value {
+		if ih.maxValue < value {
 			//add to end of array
-			additionalBins := math.Ceil((ih.MaxValue - value) / ih.BinWidth) //should take ceiling
-			newCount := len(ih.Bins) + int(additionalBins)
+			additionalBins := math.Ceil((value - ih.maxValue) / ih.binWidth) //should take ceiling
+			newCount := len(ih.bins) + int(additionalBins)
 			tmpBins := make([]int64, int(additionalBins))
-			ih.Bins = append(ih.Bins, tmpBins...)
-			ih.MaxValue = value      //should go to the end of the next whole bin
-			ih.Bins[newCount-1] += 1 //i think this is right.
+			ih.bins = append(ih.bins, tmpBins...)
+			ih.maxValue = value      //should go to the end of the next whole bin
+			ih.bins[newCount-1] += 1 //i think this is right.
 		} else {
 			//add to beginning of array
-			additionalBins := math.Ceil((value - ih.MinValue) / ih.BinWidth) //should take ceiling
+			additionalBins := math.Ceil((ih.minValue - value) / ih.binWidth) //should take ceiling
 			tmpBins := make([]int64, int(additionalBins))
-			ih.Bins = append(tmpBins, ih.Bins...)
-			ih.MinValue = value //should go to the start of the first whole bin
-			ih.Bins[0] += 1     //i think this is right.
+			ih.bins = append(tmpBins, ih.bins...)
+			ih.minValue = value //should go to the start of the first whole bin
+			ih.bins[0] += 1     //i think this is right.
 		}
 	}
+}
+func (ih *inlineHistogram) GetBins() []int64 {
+	return ih.bins
 }
--- data/inline-histogram_test.go
+++ data/inline-histogram_test.go
@@ -1,48 +1,59 @@
 package data
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+
+	"github.com/HenryGeorgist/go-statistics/statistics"
 )
 
 func TestAddObservationBinData(t *testing.T) {
-	bins := make([]int64, 5)
-	ih := InlineHistogram{Bins: bins, BinWidth: 1.0, MinValue: 0.0, MaxValue: 5.0}
+	ih := Init(1.0, 0.0, 5.0)
 	vals := []float64{.5, 1.5, 2.5, 3.5, 4.5}
 	expected := []int64{1, 1, 1, 1, 1}
 	for idx := range vals {
 		ih.AddObservation(vals[idx])
 	}
 	for idx, val := range expected {
-		if ih.Bins[idx] != val {
-			t.Errorf("Bin(%d) = %d; expected %d", idx, ih.Bins[idx], val)
+		if ih.GetBins()[idx] != val {
+			t.Errorf("Bin(%d) = %d; expected %d", idx, ih.GetBins()[idx], val)
 		}
 	}
 }
 func TestAddObservationExceedUpper(t *testing.T) {
-	bins := make([]int64, 5)
-	ih := InlineHistogram{Bins: bins, BinWidth: 1.0, MinValue: 0.0, MaxValue: 5.0}
-	vals := []float64{.5, 1.5, 2.5, 3.5, 4.5, 6.5}
+	ih := Init(1.0, 0.0, 5.0)
+	vals := []float64{.5, 1.5, 2.5, 3.5, 4.5, 5.5}
 	expected := []int64{1, 1, 1, 1, 1, 1}
 	for idx := range vals {
 		ih.AddObservation(vals[idx])
 	}
 	for idx, val := range expected {
-		if ih.Bins[idx] != val {
-			t.Errorf("Bin(%d) = %d; expected %d", idx, ih.Bins[idx], val)
+		if ih.GetBins()[idx] != val {
+			t.Errorf("Bin(%d) = %d; expected %d", idx, ih.GetBins()[idx], val)
 		}
 	}
 }
 func TestAddObservationBelowLower(t *testing.T) {
-	bins := make([]int64, 5)
-	ih := InlineHistogram{Bins: bins, BinWidth: 1.0, MinValue: 0.0, MaxValue: 5.0}
+	ih := Init(1.0, 0.0, 5.0)
 	vals := []float64{-.5, .5, 1.5, 2.5, 3.5, 4.5}
 	expected := []int64{1, 1, 1, 1, 1, 1}
 	for idx := range vals {
 		ih.AddObservation(vals[idx])
 	}
 	for idx, val := range expected {
-		if ih.Bins[idx] != val {
-			t.Errorf("Bin(%d) = %d; expected %d", idx, ih.Bins[idx], val)
+		if ih.GetBins()[idx] != val {
+			t.Errorf("Bin(%d) = %d; expected %d", idx, ih.GetBins()[idx], val)
 		}
+	}
+}
+func TestAddObservationNormalDist(t *testing.T) {
+	ih := Init(.01, -1.0, 1.0)
+	n := statistics.NormalDistribution{Mean: 0, StandardDeviation: 1}
+	for i := 0; i < 100000; i++ {
+		ih.AddObservation(n.InvCDF(rand.Float64()))
+	}
+	for _, val := range ih.GetBins() {
+		fmt.Println(val)
 	}
 }
