c79b8accb4a5640a8d351860541fa6243f312e09
rnugent3 <rnugent@gradcenter.cuny.edu>
2021-09-12 18:00:58 -0700

    wrote up beta distribution

---
+++ statistics/betaDistribution.go
@@ -0,0 +1,36 @@
+package statistics
+
+import (
+	"gonum.org/v1/gonum/stat/distuv"
+)
+
+type BetaDistribution struct {
+	Alpha float64
+	Beta  float64
+}
+
+func (d BetaDistribution) InvCDF(probability float64) float64 {
+	if probability > 1 {
+		panic("nope")
+	}
+	if probability <= 0 {
+		panic("nope")
+	}
+	b := distuv.Beta{Alpha: d.Alpha, Beta: d.Beta}
+	return b.Quantile(probability)
+}
+
+func (d BetaDistribution) CDF(value float64) float64 {
+	b := distuv.Beta{Alpha: d.Alpha, Beta: d.Beta}
+	return b.CDF(value)
+}
+
+func (d BetaDistribution) PDF(value float64) float64 {
+	b := distuv.Beta{Alpha: d.Alpha, Beta: d.Beta}
+	return b.Prob(value)
+}
+
+func (d BetaDistribution) CentralTendency() float64 {
+	b := distuv.Beta{Alpha: d.Alpha, Beta: d.Beta}
+	return b.Mean()
+}
