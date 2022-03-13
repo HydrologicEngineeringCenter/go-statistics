ea9ae3bfbe4043f8ed0fa6e263519f04f95c7941
Will Lehman <william.p.lehman@usace.army.mil>
2022-03-13 17:39:27 +0000

    adding a reference to rand

--- statistics/logpearsoniiiDistribution.go
+++ statistics/logpearsoniiiDistribution.go
@@ -2,20 +2,22 @@ package statistics
 
 import (
 	"math"
+	"math/rand"
 
 	"github.com/HydrologicEngineeringCenter/go-statistics/data"
 )
 
 type LogPearsonIIIDistribution struct {
-	Mean              float64 `json:"mean"`
-	StandardDeviation float64 `json:"standarddeviation"`
-	Skew              float64 `json:"skew"`
-	EquivalentYearsOfRecord int `json:"equivalent_years_of_record"`
+	Mean                    float64 `json:"mean"`
+	StandardDeviation       float64 `json:"standarddeviation"`
+	Skew                    float64 `json:"skew"`
+	EquivalentYearsOfRecord int     `json:"equivalent_years_of_record"`
 }
-func (n LogPearsonIIIDistribution) Bootstrap(seed int64) ContinuousDistribution{
+
+func (n LogPearsonIIIDistribution) Bootstrap(seed int64) ContinuousDistribution {
 	rnd := rand.New(rand.NewSource(seed))
 	samples := make([]float64, n.EquivalentYearsOfRecord)
-	for i:=0; i<n.EquivalentYearsOfRecord; i ++{
+	for i := 0; i < n.EquivalentYearsOfRecord; i++ {
 		samples[i] = n.InvCDF(rnd.Float64())
 	}
 	ret := n.Fit(samples)
@@ -30,9 +32,9 @@ func (n LogPearsonIIIDistribution) Fit(inputData []float64) ContinuousDistributi
 	}
 	pm.AddObservations(logData)
 	ret := LogPearsonIIIDistribution{
-		Mean: pm.GetMean(),
+		Mean:              pm.GetMean(),
 		StandardDeviation: pm.GetSampleVariance(),
-		Skew: n.Skew,//TO DO: compute skew.
+		Skew:              n.Skew, //TO DO: compute skew.
 	}
 	return ret
 }
